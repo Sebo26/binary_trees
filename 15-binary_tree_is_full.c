52b0a5e793479104bffacc618dbca91e8a217ed6
root <sebomaduna26@gmail.com>
2024-02-29 17:07:32 -0800

    No 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,6 +10,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	{
 		return (0);
 	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
 	binary_tree_is_full(tree->left);
 	binary_tree_is_full(tree->right);
 	return (1);
